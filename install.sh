f24e555a300553dd59ed31c060edd0d52436d72e
Matthieu Dufour <matthieu.dufour@proton.me>
2025-05-04 21:30:50 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+## Plugins:
+# aliases dnf git pip python
+
+echo 'export PIP_REQUIRE_VIRTUALENV=true\n\n' >> .zshrc
+
+
 # Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
